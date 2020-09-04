aff71578f3eef92587ff6bf5743cb1ccceb176e0
Armstrong, Spencer M <spencer-armstrong@uiowa.edu>
2020-09-04 18:18:08 -0500

    day 1 finished up
    renamed images and made a couple changes to set.py

--- set.py
+++ set.py
@@ -1,3 +1,6 @@
+import os
+import sys
+
 class Deck:
 
     cards = {}
@@ -6,15 +9,16 @@ class Deck:
     # the deck via a comprehension that assigns the key of each card to be a unique four 
     # digit code and the value is the card object itself.
     def __init__(self):
-        images = {}
-
+        
         for n in range(1,4):
             for s in range(1,4):
                 for f in range(1,4):
                     for c in range(1,4):
-                        self.cards[str(n) + str(s) + str(f) + str(c)] = Card(n,s,f,c, images[str(n) + str(s) + str(f) + str(c)])
+                        self.cards[str(n) + str(s) + str(f) + str(c)] = Card(n,s,f,c, str(n) + str(s) + str(f) + str(c))
 
 class Card:
+
+
 
     def __init__(self, num, shape, fill, color, image):
         shapes = {1 : "Diamond", 2 : "Oval", 3 : "Squiggle"}
@@ -25,7 +29,11 @@ class Card:
         self.shape = shapes[shape]
         self.fill = fills[fill]
         self.color = colors[color]
+        self.image = images[image]
 
+images = {}
+for image in pathlib.Path("DeckoCards").iterDir():
+    images[image.replace('.png','')]
 
 deck = Deck()
 print(deck.cards['1111'].shape)
